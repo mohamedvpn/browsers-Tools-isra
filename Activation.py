acd568fb9179d8bd8d5e66ca2d8b258ad1418ca1
mohamedvpn <103552750+mohamedvpn@users.noreply.github.com>
2024-12-10 22:44:08 +0200

    Create Activation.py

---
+++ Activation.py
@@ -0,0 +1,4 @@
+{
+  "6JJ8-DGGR-3ZD4-YTPL":{'name':'Mohamed Gamal','mac':'e4a3da37ff5f9b92d209509d19b5579b09347c1e4b401f10c5c7bc2c4c8dbc3ed8d6S4n0'},
+  "6JJ2H8-DGGRd-3ZD4s-Y4TP6L":{'name':'Nader Abo Bakr','mac':'f524636b48f4450635c4661cf2f1a19898f9ab4f91ef4db94d7d8fff442fbd0ad8d6S4n0'}
+}
